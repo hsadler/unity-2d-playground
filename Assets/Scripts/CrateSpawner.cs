948f8b2210409304936acc6674f8a7a9a4affe59
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-05 18:18:17 -0800

    first commit

---
+++ Assets/Scripts/CrateSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateSpawner : MonoBehaviour
+{
+    
+    public GameObject cratePink;
+    public int waitSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("WaitAndSpawnCrate", 0.0f, 0.5f);
+    }
+
+    void WaitAndSpawnCrate()
+    {
+        Instantiate(cratePink, transform.position, transform.rotation, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
