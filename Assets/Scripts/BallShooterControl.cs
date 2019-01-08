d6fa3e56bf601be399be012a3cd7df9cb3abbd0e
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-07 20:35:21 -0800

    add new scene with orange triangle control like asteroid space ship

---
+++ Assets/Scripts/BallShooterControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooterControl : MonoBehaviour
+{
+
+    public float thrust;
+    public float torque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * thrust);
+        }
+        if(Input.GetKey(KeyCode.S)) {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * -thrust);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            GetComponent<Rigidbody2D>().AddTorque(torque);
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            GetComponent<Rigidbody2D>().AddTorque(-torque);
+        }
+    }
+}
