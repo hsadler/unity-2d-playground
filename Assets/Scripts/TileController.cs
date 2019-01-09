07c22e409576f3caef4f478d0c78c78581e46c18
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-08 21:25:31 -0800

    add tile factory game scene of proceedurally generated game tiles

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+
+    private GameObject rootGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rootGO = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseDown()
+    {
+        Destroy(rootGO);
+    }
+
+}
---
+++ Assets/Scripts/TileFactoryGameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileFactoryGameManager : MonoBehaviour
+{
+
+    public static TileFactoryGameManager instance { get; private set; }
+
+    public GameObject gameTilePrefab;
+
+    public static IDictionary<int[], GameObject> tiles;
+    public int tilesWidth;
+    public int tilesHeight;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        // singleton pattern
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = -tilesHeight; y < tilesHeight; y++)
+        {
+            for (int x = -tilesWidth; x < tilesWidth; x++)
+            {
+                Vector3 position = new Vector3(x, y, 0);
+                Instantiate(gameTilePrefab, position, transform.rotation);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
