584b6ec12fe2f0e84a3f1583e0d4a7a1ad25f8a6
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-11 23:54:17 -0800

    start work on menu for changing scenes

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public static MenuManager instance { get; private set; }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        // singleton pattern
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void GoToBoilingSquaresScene() {
+        SceneManager.LoadScene("BoilingSquares");
+    }
+}
