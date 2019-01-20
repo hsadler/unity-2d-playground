a5ad0ac3642e04e1362732b11001e4fb26f1bcd9
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-19 19:35:44 -0800

    add PumpBalloon scene

---
+++ Assets/Scripts/BalloonControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonControl : MonoBehaviour
+{
+ 
+    public float pumpForceRate;
+    public float pumpScaleRate;
+    
+    private bool doPump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doPump) {
+            PumpBalloon();
+        }
+    }
+
+    void PumpBalloon() {
+        Transform t = transform;
+        ConstantForce2D cf = gameObject.GetComponent<ConstantForce2D>();
+        // scale balloon larger
+        t.localScale += new Vector3(pumpScaleRate, pumpScaleRate, 0);
+        // add upward force to balloon
+        cf.force += new Vector2(0, pumpForceRate);
+    }
+
+    public void PumpOn() {
+        doPump = true;
+    }    
+
+    public void PumpOff() {
+        doPump = false;
+    }
+
+}
---
+++ Assets/Scripts/PumpBalloonButtonControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PumpBalloonButtonControl : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    
+    public GameObject balloon;
+    
+    public void OnPointerDown(PointerEventData eventData) {
+        balloon.GetComponent<BalloonControl>().PumpOn();
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        balloon.GetComponent<BalloonControl>().PumpOff();
+    }
+
+}
