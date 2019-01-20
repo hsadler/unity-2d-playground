0dd320a22eb78b7909fc4238063b75ad89f1ddb0
Harry Sadler <harrysadlermusic@gmail.com>
2019-01-20 14:03:24 -0800

    add Triggers scene

---
+++ Assets/Scripts/SpringTriggerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringTriggerControl : MonoBehaviour
+{
+
+    private HingeJoint2D plankJoint;
+    private HingeJoint2D groundJoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HingeJoint2D[] hjs = GetComponents<HingeJoint2D>();
+        plankJoint = hjs[0];
+        groundJoint = hjs[1];
+    }
+
+    public void ReleaseTrigger() {
+        Destroy(plankJoint);
+    }
+
+}
